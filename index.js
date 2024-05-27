c169c62a7d0f4ebecbc70e74933d6e061557fd4d
Francois Chenebit <francois.chenebit@laposte.net>
2024-05-27 22:54:26 +0200

    adding log message

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 80;
 
+console.log(`using port : ${PORT}`);
+
 app.use(express.static('site'));
 
 app.listen(PORT, () => {
